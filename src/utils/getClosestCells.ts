e977781eff1d43c143ca2c9bb95962e3541110b9
SemenovBohdan123 <nadgbosemenov@gmail.com>
2023-03-24 17:15:52 +0200

    fixes main logic getClosesrCells

--- src/utils/getClosestCells.ts
+++ src/utils/getClosestCells.ts
@@ -8,17 +8,26 @@ const getClosestCells = (
   }
 
   const hoveredAmount = hoveredCell.amount;
+  const matrixSize = matrix.length;
+
+  const rowIndex = matrix.findIndex((row) => row.includes(hoveredCell));
+  const colIndex = matrix[rowIndex].indexOf(hoveredCell);
+
+  const startRow = Math.max(rowIndex - 3, 0);
+  const endRow = Math.min(rowIndex + 3, matrixSize - 1);
+  const startCol = Math.max(colIndex - 3, 0);
+  const endCol = Math.min(colIndex + 3, matrixSize - 1);
 
   const differences: IDifferencesCell[] = [];
 
-  matrix.flat().forEach((item) => {
-    if (item.id < hoveredCell.id) {
-      return;
-    }
-
-    differences.push({
-      ...item,
-      difference: Math.abs(item.amount - hoveredAmount),
+  matrix.forEach((row, i) => {
+    if (i < startRow || i > endRow) return;
+    row.forEach((item, j) => {
+      if (j < startCol || j > endCol || item.id < hoveredCell.id) return;
+      differences.push({
+        ...item,
+        difference: Math.abs(item.amount - hoveredAmount),
+      });
     });
   });
 
